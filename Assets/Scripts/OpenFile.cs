2a250edfe2da478170bfa1345f48211d2e9409dd
adamsandlerisfunny <lnicolaidou.katerina@gmail.com>
2019-10-25 13:56:11 +0200

    Saving

---
+++ Assets/Scripts/OpenFile.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class OpenFile : EditorWindow
+{
+    [MenuItem("Window/File Browser")]
+    public static string SelectFilePath()
+    {
+        string path = EditorUtility.OpenFilePanel("Select a map file", SaveSystem.SAVE_FOLDER, "json");
+        if (path.Length != 0)
+        {
+            return path;
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class SaveSystem
+{
+    public static readonly string SAVE_FOLDER = Application.dataPath + "/Saves/";
+
+    public static void Init(){
+        if (!Directory.Exists(SAVE_FOLDER)){
+            Directory.CreateDirectory(SAVE_FOLDER);
+        }
+    }
+    public static void Save(string saveString){
+        File.WriteAllText(SAVE_FOLDER + EditorManager.em.DataFileName, saveString);
+    }
+
+    public static string Load(){
+        if(File.Exists(SAVE_FOLDER + EditorManager.em.DataFileName))
+        {
+            // Read the json from the file into a string
+            //string saveString = File.ReadAllText(SAVE_FOLDER + EditorManager.em.DataFileName);
+            string saveString = File.ReadAllText(OpenFile.SelectFilePath());
+            if (saveString.Length != 0)
+                return saveString;
+        } else
+        {
+            Debug.Log("There are no saved files.");
+        }
+        return null;
+    }
+}
