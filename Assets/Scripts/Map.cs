2e16625c926ae8423748129805af3d11d74a87bc
kathy <lnicolaidou.katerina@gmail.com>
2019-11-10 17:54:17 +0100

    People Spawn and UI Management

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map
+{
+    public float MapValue(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
