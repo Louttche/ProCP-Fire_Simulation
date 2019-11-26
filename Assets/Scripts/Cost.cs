8d2bcb71fc2f3187d346cc5a45b11a8d20588f04
kathy <lnicolaidou.katerina@gmail.com>
2019-11-26 11:33:07 +0100

    Budget and Cost

---
+++ Assets/Scripts/Cost.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cost : MonoBehaviour
+{
+    public static Cost c;
+    public float budget, totalCost;
+
+    private void Awake() {
+        c = this;
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/Scripts/Managers/Edit_UIManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edit_UIManager : MonoBehaviour
+{
+    public static Edit_UIManager cuim;
+    public TMPro.TextMeshProUGUI lbl_totalCost, lbl_budget;
+
+    private void Awake() {
+        cuim = this;
+    }
+
+    private void Update() {
+        if (MapGrid_Flex.mg.currentTiles != null){
+            UpdateTotalCost();
+            lbl_budget.text = Cost.c.budget.ToString();
+            /*if (int.Parse(lbl_budget.GetComponent<TMPro.TextMeshProUGUI>().text) < int.Parse(lbl_totalCost.GetComponent<TMPro.TextMeshProUGUI>().text)){
+                lbl_totalCost.GetComponent<Image>().color = Color.red;
+            } else
+                lbl_totalCost.GetComponent<Image>().color = Color.black;*/
+        }
+    }
+
+    public void UpdateTotalCost(){
+        Cost.c.totalCost = 0;
+        foreach (var t in MapGrid_Flex.mg.currentTiles)
+        {
+            Cost.c.totalCost += t.cost;
+            lbl_totalCost.text = Cost.c.totalCost.ToString();
+        }
+    }
+}
